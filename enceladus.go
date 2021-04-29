3297288dee2f04994c633f65af59df1b18e44c65
mail@thierry-decker.com <Themis@0710>
2021-04-29 08:53:33 +0200

    Split capture statistics into a new file

--- enceladus.go
+++ enceladus.go
@@ -177,38 +177,6 @@ func main() {
 	}
 }
 
-func captureStats(d <-chan bool, handle *pcap.Handle, interval time.Duration, l *zap.SugaredLogger) {
-	/*
-		Log the capture statistics
-	*/
-	defer wgCaptureStatsRunning.Done()
-	ticker := time.NewTicker(interval)
-	l.Debug("Capture statistics: running")
-	wgCaptureStatsPending.Done()
-	for {
-		select {
-		case _ = <-d:
-			l.Debug("Capture statistics: Stopping...")
-			return
-		case _ = <-ticker.C:
-			stats, _ := handle.Stats()
-			received := uint64(stats.PacketsReceived)
-			dropped := uint64(stats.PacketsDropped)
-			droppedPercent := (float64(dropped) / float64(received)) * 100
-			ifDropped := uint64(stats.PacketsIfDropped)
-			if dropped == 0 && ifDropped == 0 {
-				l.Infof("Statistics: Received %v, dropped %v (%.3f %%) and ifdropped %v packets", received, dropped,
-					droppedPercent, ifDropped)
-			} else {
-				l.Warnf("Statistics: Received %v, dropped %v (%.3f %%) and ifdropped %v packets", received, dropped,
-					droppedPercent, ifDropped)
-			}
-		default:
-			time.Sleep(conf.ttlInterval)
-		}
-	}
-}
-
 func handlePacket(p <-chan gopacket.Packet, ptc chan<- gopacket.Packet, d <-chan bool, l *zap.SugaredLogger, n int) {
 	defer wgPacketHandlerRunning.Done()
 	l.Debugf("Packet handling %v: running", n)
