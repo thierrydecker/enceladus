0e458b46a8a388edc7cb6369de9ec23a7918df28
mail@thierry-decker.com <Themis@0710>
2021-04-29 08:56:31 +0200

    Split packet decoders into a new file

---
+++ decoders.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/log"
+	"go.uber.org/zap"
+)
+
+func decodePacket(p <-chan gopacket.Packet, d <-chan bool, l *zap.SugaredLogger, n int) {
+	defer wgPacketDecoderRunning.Done()
+	l.Debugf("Packet decoding %v: running", n)
+	/*
+		Create InfluxDb client
+	*/
+	client := influxdb2.NewClientWithOptions(
+		confDb.url,
+		confDb.token,
+		influxdb2.DefaultOptions().
+			SetPrecision(time.Nanosecond),
+	)
+	/*
+		Disable InfluxDb logging
+	*/
+	log.Log = nil
+	writeAPI := client.WriteAPI(confDb.org, confDb.bucket)
+	errorsCh := writeAPI.Errors()
+	go func(l *zap.SugaredLogger) {
+		for err := range errorsCh {
+			l.Errorf("Packet decoding %v: Write to InfluxDb Error %v", n, err)
+		}
+	}(l)
+	defer client.Close()
+	defer writeAPI.Flush()
+	wgPacketDecoderPending.Done()
+	for {
+		select {
+		case _ = <-d:
+			l.Debugf("Packet decoding %v: Stopping...", n)
+			return
+		case packet := <-p:
+			/*
+				Common fields
+			*/
+			packetTimestamp := time.Now()
+			packetLength := len(packet.Data())
+			/*
+				Decode ethernet layer if present
+			*/
+			ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+			if ethernetLayer != nil {
+				/*
+					Ethernet fields
+				*/
+				ethernet, _ := ethernetLayer.(*layers.Ethernet)
+				srcMac := ethernet.SrcMAC
+				dstMac := ethernet.SrcMAC
+				ethernetType := ethernet.EthernetType
+				msg := fmt.Sprintf("Packet decoding %v: Received Ethernet frame, ", n)
+				msg += fmt.Sprintf("timestamp: %v, ", packetTimestamp)
+				msg += fmt.Sprintf("packetLength: %v, ", packetLength)
+				msg += fmt.Sprintf("srcMac: %v, ", srcMac)
+				msg += fmt.Sprintf("dstMac: %v, ", dstMac)
+				msg += fmt.Sprintf("ethernetType: %v", ethernetType)
+				l.Debug(msg)
+				/*
+					Send data to InfluxDb
+				*/
+				point := influxdb2.NewPoint(
+					"Ethernet",
+					map[string]string{
+						"agent":        confDb.agent,
+						"ethernetType": ethernetType.String(),
+					},
+					map[string]interface{}{
+						"srcMac":       srcMac,
+						"srcDst":       dstMac,
+						"packetLength": packetLength,
+					},
+					packetTimestamp,
+				)
+				writeAPI.WritePoint(point)
+			} else {
+				msg := fmt.Sprintf("Packet decoding %v: Received unknown message, ", n)
+				msg += fmt.Sprintf("timestamp: %v, ", packetTimestamp)
+				msg += fmt.Sprintf("packetLength: %v, ", packetLength)
+				l.Warn(msg)
+			}
+		default:
+			time.Sleep(conf.ttlInterval)
+		}
+	}
+}
